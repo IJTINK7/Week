a278ccad4b2cb98321a13b9197f0d7489750aced
IJTINK7 <ijtink7@mail.ru>
2022-11-12 04:19:39 +0300

    Create "splitToWords" function

--- Monday/native-js-ts/src/01-hello-tests/01.ts
+++ Monday/native-js-ts/src/01-hello-tests/01.ts
@@ -5,3 +5,7 @@ export const sum = (a:number, b: number) => {
 export const multiply = (a:number, b: number) => {
 	return a * b;
 }
+
+export const splitToWords = (sentence: string) =>{
+	return ["Hello", "my", "friend"];
+}
