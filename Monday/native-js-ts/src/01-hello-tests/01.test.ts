c971b81823c3c7e3291a6e70bb3c8f9a6b38d8b6
IJTINK7 <ijtink7@mail.ru>
2022-11-12 04:00:11 +0300

    Adding comments

--- Monday/native-js-ts/src/01-hello-tests/01.test.ts
+++ Monday/native-js-ts/src/01-hello-tests/01.test.ts
@@ -1,28 +1,30 @@
 import {multiply, sum} from "./01";
 
-test('sum should be correct', ()=>{
+test('sum should be correct', () => { // 2 arguments - name of test and function with three phases: 1)data; 2)action; 3)expect;
+	//data
 	const a = 1;
 	const b = 2;
 	const c = 3;
-
-	const result1 = sum(a,b);
-	const result2 = sum(b,c);
-	const result3 = sum(a,c);
-
+	//action
+	const result1 = sum(a, b);
+	const result2 = sum(b, c);
+	const result3 = sum(a, c);
+	//expect
 	expect(result1).toBe(3);
 	expect(result2).toBe(5);
 	expect(result3).toBe(4);
-});
+}); // yarn start
 
-test('result should be correct', ()=>{
+test('result should be correct', () => {
+	//data
 	const a = 3;
 	const b = 4;
 	const c = 5;
-
-	const result4 = multiply(a,b);
-	const result5 = multiply(b,c);
-	const result6 = multiply(a,c);
-
+	//action
+	const result4 = multiply(a, b);
+	const result5 = multiply(b, c);
+	const result6 = multiply(a, c);
+	//expect
 	expect(result4).toBe(12);
 	expect(result5).toBe(20);
 	expect(result6).toBe(15);
