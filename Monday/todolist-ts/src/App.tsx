989640cfb328ccf73be24e32639da8b2948adbd6
IJTINK7 <ijtink7@mail.ru>
2022-11-17 03:50:55 +0300

    Add function "removeTask"

--- Monday/todolist-ts/src/App.tsx
+++ Monday/todolist-ts/src/App.tsx
@@ -1,34 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {ToDoList} from './ToDoList';
 
 
 export function App() {
-	let tasks = [
+	// let tasks = [
+	// 	{id: 1, checked: true, name: "HTML"},
+	// 	{id: 2, checked: true, name: "CSS"},
+	// 	{id: 3, checked: true, name: "JS"},
+	// 	{id: 4, checked: false, name: "React"},
+	// 	{id: 5, checked: false, name: "TS"},
+	// ];
+	let[tasks, setTasks] = useState([
 		{id: 1, checked: true, name: "HTML"},
 		{id: 2, checked: true, name: "CSS"},
 		{id: 3, checked: true, name: "JS"},
 		{id: 4, checked: false, name: "React"},
 		{id: 5, checked: false, name: "TS"},
-	];
-	let movies = [
-		{id: 1, checked: true, name: "Titanic"},
-		{id: 2, checked: true, name: "Avatar"},
-		{id: 3, checked: false, name: "Avengers"},
-		{id: 4, checked: false, name: "Aviator"},
-	];
-	let songs = [
-		{id: 1, checked: false, name: "Hurt"},
-		{id: 2, checked: false, name: "Pretty Woman"},
-		{id: 3, checked: true, name: "She's a rebel"},
-		{id: 4, checked: true, name: "Holiday"},
-	];
+	])
+
+	const removeTask =(taskId:number)=>{
+		setTasks(tasks.filter(el => el.id !== taskId));
+	}
 
 	return (
 		<div className="App">
-			<ToDoList title={"What to learn"} list={tasks} />
-			<ToDoList title={"James Cameron Movies"} list={movies}/>
-			<ToDoList title={"Green Day Songs"} list={songs}/>
+			<ToDoList title={"What to learn"} list={tasks} removeTask={removeTask} />
 		</div>
 	);
 }
--- Monday/todolist-ts/src/ToDoList.tsx
+++ Monday/todolist-ts/src/ToDoList.tsx
@@ -3,6 +3,7 @@ import React from "react";
 export type TitlePropsType = {
 	title: string;
 	list: Array<ListPropsType>;
+	removeTask:(taskId:number)=>void;
 }
 
 export type ListPropsType = {
@@ -12,15 +13,22 @@ export type ListPropsType = {
 }
 
 export const ToDoList = (props: TitlePropsType) => {
-	debugger
 	return (
 		<div>
 			<h3>{props.title}</h3>
 			<input/>
 			<button>+</button>
 			<ul>
-				{props.list.map((el) => <li key={el.id}><input type="checkbox"
-				checked={el.checked}/><span>{el.name}</span></li>)}
+				{props.list.map((el) => {
+					return (
+						<li key={el.id}>
+							<button onClick={() => {props.removeTask(el.id)}}>X
+							</button>
+							<input type="checkbox" checked={el.checked}/>
+							<span>{el.name}</span>
+						</li>
+					)
+				})}
 			</ul>
 			<div>
 				<button>All</button>
