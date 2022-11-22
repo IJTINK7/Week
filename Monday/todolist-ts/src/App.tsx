8907bb7c191bf3df78bc261658bdd1dd321718b6
IJTINK7 <ijtink7@mail.ru>
2022-11-22 17:12:14 +0300

    Clearing "App.tsx" and "ToDoList.tsx" files for useState hook and filter method training

--- Monday/todolist-ts/src/App.tsx
+++ Monday/todolist-ts/src/App.tsx
@@ -4,37 +4,13 @@ import {ToDoList} from './ToDoList';
 
 
 export function App() {
-	// let tasks = [
-	// 	{id: 1, checked: true, name: "HTML"},
-	// 	{id: 2, checked: true, name: "CSS"},
-	// 	{id: 3, checked: true, name: "JS"},
-	// 	{id: 4, checked: false, name: "React"},
-	// 	{id: 5, checked: false, name: "TS"},
-	// ];
-	let[tasks, setTasks] = useState([
+	let tasks = [
 		{id: 1, checked: true, name: "HTML"},
 		{id: 2, checked: true, name: "CSS"},
 		{id: 3, checked: true, name: "JS"},
 		{id: 4, checked: false, name: "React"},
 		{id: 5, checked: false, name: "TS"},
-	])
-
-	const removeTask =(taskId:number)=>{
-		setTasks(tasks.filter(el => el.id !== taskId));
-	}
-	const [filterValueKey, setFilterValueKey] = useState("All")
-
-
-	let filteredTasks = tasks;
-	const filterTasks =(filterKey:string)=>{
-		setFilterValueKey(filterKey)
-	}
-	if(filterValueKey === "Active"){
-		filteredTasks = tasks.filter(el=>el.checked)
-	}
-	if(filterValueKey === "Completed"){
-		filteredTasks = tasks.filter(el=>!el.checked)
-	}
+	];
 
 	let [count, setCount] = useState(1);
 	const onClickHandler = () => {
@@ -42,7 +18,7 @@ export function App() {
 	}
 	return (
 		<div className="App">
-			<ToDoList title={"What to learn"} list={filteredTasks} removeTask={removeTask} filterTasks={filterTasks} />
+			<ToDoList title={"What to learn"} list={tasks}/>
 			<div>
 				<p>Вы кликнули {count} раз(а)</p>
 				<button onClick={onClickHandler}>Нажми на меня</button>
