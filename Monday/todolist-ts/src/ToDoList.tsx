b230379be282f695d078f8f189dbf1b91f0fd09f
IJTINK7 <ijtink7@mail.ru>
2023-03-21 23:38:02 +0300

    Adding props to tasks of each todolist

--- Monday/todolist-ts/src/ToDoList.tsx
+++ Monday/todolist-ts/src/ToDoList.tsx
@@ -19,10 +19,10 @@ export const ToDoList = (props: PropsType) => {
 				<button>+</button>
 			</div>
 			<ul>
-				<li><input type="checkbox" checked={true}/><span>HTML</span></li>
-				<li><input type="checkbox" checked={true}/><span>CSS</span></li>
-				<li><input type="checkbox" checked={true}/><span>JS</span></li>
-				<li><input type="checkbox" checked={false}/><span>React</span></li>
+				<li><input type="checkbox" checked={props.tasks[0].isDone}/><span>{props.tasks[0].title}</span></li>
+				<li><input type="checkbox" checked={props.tasks[1].isDone}/><span>{props.tasks[1].title}</span></li>
+				<li><input type="checkbox" checked={props.tasks[2].isDone}/><span>{props.tasks[2].title}</span></li>
+				<li><input type="checkbox" checked={props.tasks[3].isDone}/><span>{props.tasks[3].title}</span></li>
 			</ul>
 			<div>
 				<button>All</button>
