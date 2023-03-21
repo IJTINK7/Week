5e45ca5647eb305f4864d45842cbfef0b1be97cc
IJTINK7 <ijtink7@mail.ru>
2023-03-21 23:34:00 +0300

    Adding "tasks" and "movies" arrays

--- Monday/todolist-ts/src/App.tsx
+++ Monday/todolist-ts/src/App.tsx
@@ -3,18 +3,18 @@ import './App.css';
 import {ToDoList} from "./ToDoList";
 
 function App() {
-    // let tasks: Array<TasksType> = [
-    //     {id: 1, title: "HTML", isDone: true},
-    //     {id: 2, title: "CSS", isDone: true},
-    //     {id: 3, title: "JS", isDone: true},
-    //     {id: 4, title: "React", isDone: false},
-    // ]
-    // let movies: Array<TasksType> = [
-    //     {id: 1, title: "Avatar", isDone: true},
-    //     {id: 2, title: "Titanic", isDone: true},
-    //     {id: 3, title: "Insidious", isDone: false},
-    //     {id: 4, title: "Terminator 2", isDone: true},
-    // ]
+    let tasks = [
+        {id: 1, title: "HTML", isDone: true},
+        {id: 2, title: "CSS", isDone: true},
+        {id: 3, title: "JS", isDone: true},
+        {id: 4, title: "React", isDone: false},
+    ]
+    let movies = [
+        {id: 1, title: "Avatar", isDone: true},
+        {id: 2, title: "Titanic", isDone: true},
+        {id: 3, title: "Insidious", isDone: false},
+        {id: 4, title: "Terminator 2", isDone: true},
+    ]
   return (
     <div className="App">
       <ToDoList />
