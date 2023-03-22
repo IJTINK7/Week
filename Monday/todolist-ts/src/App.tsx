f2e4ebd872fce6721afd96079b629fd98b1ab1c3
IJTINK7 <ijtink7@mail.ru>
2023-03-22 16:20:58 +0300

    Changing tasks array via "useState" hook

--- Monday/todolist-ts/src/App.tsx
+++ Monday/todolist-ts/src/App.tsx
@@ -1,20 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {ToDoList} from "./ToDoList";
 
 function App() {
-    let tasks = [
+    let [tasks, setTasks] = useState([
         {id: 1, title: "HTML", isDone: true},
         {id: 2, title: "CSS", isDone: true},
         {id: 3, title: "JS", isDone: true},
         {id: 4, title: "React", isDone: false},
-    ]
-    let movies = [
-        {id: 1, title: "Avatar", isDone: true},
-        {id: 2, title: "Titanic", isDone: true},
-        {id: 3, title: "Insidious", isDone: false},
-        {id: 4, title: "Terminator 2", isDone: true},
-    ]
+    ])
     const removeTask = (id: number) => {
         tasks = tasks.filter(el=> el.id !== id)
     }
@@ -24,11 +18,6 @@ function App() {
         title = {"What to learn"}
         tasks = {tasks}
         removeTask={removeTask}
-      />
-      <ToDoList
-          title = {"James Cameron's movies"}
-          tasks = {movies}
-          removeTask={removeTask}
       />
     </div>
   );
