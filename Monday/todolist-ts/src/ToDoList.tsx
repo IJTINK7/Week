f74e9b7a098a811880fb21e79f61ac2ce0d5c6b4
IJTINK7 <ijtink7@mail.ru>
2023-03-22 16:38:56 +0300

    Completed "Todolist 02" lesson

--- Monday/todolist-ts/src/ToDoList.tsx
+++ Monday/todolist-ts/src/ToDoList.tsx
@@ -71,3 +71,42 @@ export const ToDoList = (props: PropsType) => {
 // 		</div>
 // 	);
 // }
+
+// Todolist 02
+// import React from "react";
+// import {FilterValuesType} from "./App";
+//
+// type PropsType = {
+// 	title: string;
+// 	tasks: Array<TasksType>;
+// 	removeTask: Function;
+// 	changeFilter: (value: FilterValuesType)=> void;
+// }
+// export type TasksType ={
+// 	id: number;
+// 	title: string;
+// 	isDone: boolean;
+// }
+//
+// export const ToDoList = (props: PropsType) => {
+// 	return (
+// 		<div>
+// 			<h3>{props.title}</h3>
+// 			<div>
+// 				<input />
+// 				<button>+</button>
+// 			</div>
+// 			<ul>
+// 				{props.tasks.map(el=> <li><input type="checkbox" checked={el.isDone}/>
+// 					<span>{el.title}</span>
+// 					<button onClick={()=>{props.removeTask(el.id)}}>x</button>
+// 				</li>)}
+// 			</ul>
+// 			<div>
+// 				<button onClick={()=>{props.changeFilter('all')}}>All</button>
+// 				<button onClick={()=>{props.changeFilter('active')}}>Active</button>
+// 				<button onClick={()=>{props.changeFilter('completed')}}>Complete</button>
+// 			</div>
+// 		</div>
+// 	);
+// }
