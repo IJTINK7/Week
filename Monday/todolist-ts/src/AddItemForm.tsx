7d27f316c1a16406b72e1fd499171ff2067368d6
IJTINK7 <ijtink7@mail.ru>
2023-04-10 22:01:24 +0300

    Transferring necessary functions to AddItemForm.tsx component for its correct working

--- Monday/todolist-ts/src/AddItemForm.tsx
+++ Monday/todolist-ts/src/AddItemForm.tsx
@@ -1,5 +1,25 @@
-import React from "react";
-export const addTaskHandler = () => {
+import React, {ChangeEvent, KeyboardEvent, useState} from "react";
+export const AddItemForm = () => {
+	let [newTaskTitle, setNewTaskTitle] = useState("")
+	let [error, setError] = useState<null | string>(null);
+	const onNewTitleChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+		setNewTaskTitle(e.currentTarget.value)
+	}
+	const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+		setError(null)
+		if (e.ctrlKey && e.charCode === 13) {
+			props.addTask(props.id, newTaskTitle)
+			setNewTaskTitle("")
+		}
+	}
+	const addTaskHandler = () => {
+		if (newTaskTitle.trim() !== "") {
+			props.addTask(props.id, newTaskTitle.trim())
+			setNewTaskTitle("")
+		} else {
+			setError("Field is required")
+		}
+	}
 	return (
 		<div>
 			<input
