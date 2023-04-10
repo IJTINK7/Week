d8bcad1bc392e6cc1e8f0927bdbc5a2dd1976dc5
IJTINK7 <ijtink7@mail.ru>
2023-04-10 21:28:32 +0300

    Copying input and button elements from ToDoList.tsx component to "AddItemForm" component

--- Monday/todolist-ts/src/AddItemForm.tsx
+++ Monday/todolist-ts/src/AddItemForm.tsx
@@ -1,41 +1,16 @@
-import React, {ChangeEvent, KeyboardEvent, useState} from "react";
-//
-// type AddItemFormPropsType = {
-// 	addItem: (newTaskTitle: string) => void;
-// }
-// export const AddItemForm = (props: AddItemFormPropsType) => {
-// 	let [newTaskTitle, setNewTaskTitle] = useState("")
-// 	let [error, setError] = useState<null | string>(null)
-// 	const onNewTitleChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-// 		setNewTaskTitle(e.currentTarget.value)
-// 	}
-// 	const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
-// 		setError(null)
-// 		if (e.ctrlKey && e.charCode === 13) {
-// 			props.addItem(newTaskTitle)
-// 			setNewTaskTitle("")
-// 		}
-// 	}
-	const addTaskHandler = () => {
-// 		if (newTaskTitle.trim() !== "") {
-// 			props.addItem(newTaskTitle.trim())
-// 			setNewTaskTitle("")
-// 		} else {
-// 			setError("Field is required")
-// 		}
-// 	}
+import React from "react";
+	export const addTaskHandler = () => {
 	return (
-		<div>Hi</div>
-// 		<div>
-// 			<input
-// 				value={newTaskTitle}
-// 				onChange={onNewTitleChangeHandler}
-// 				onKeyPress={onKeyPressHandler}
-// 				className={error ? "error" : ""}
-// 			/>
-// 			<button onClick={addTaskHandler}>+</button>
-// 			{error && <div className={"error-message"}>{error}</div>}
-// 		</div>
+		<div>
+			<input
+				value={newTaskTitle}
+				onChange={onNewTitleChangeHandler}
+				onKeyPress={onKeyPressHandler}
+				className={error ? "error" : ""}
+			/>
+			<button onClick={addTaskHandler}>+</button>
+			{error && <div className={"error-message"}>{error}</div>}
+		</div>
 	);
 }
 
