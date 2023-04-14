387a0d89ea19afe883df223f94245d52451693ca
IJTINK7 <ijtink7@mail.ru>
2023-04-14 13:20:11 +0300

    Adding array of movies

--- SamuraiWillHelp/movielist/src/App.tsx
+++ SamuraiWillHelp/movielist/src/App.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import {v1} from "uuid";
 
 function App() {
+    const [movies, setMovies] = useState([
+        {id: v1(), name: 'The Shawshank Redemption', watched: false, rating: 93},
+        {id: v1(), name: 'The Godfather', watched: false, rating: 92},
+        {id: v1(), name: 'The Dark Knight', watched: false, rating: 91},
+        {id: v1(), name: 'The Godfather Part II', watched: false, rating: 90},
+        {id: v1(), name: 'Schindler\'s List', watched: false, rating: 89},
+        {id: v1(), name: 'The Lord of the Rings', watched: false, rating: 89},
+        {id: v1(), name: 'Pulp Fiction', watched: false, rating: 89}
+    ])
   return (
     <div className="App">
       <header className="App-header">
-        <div className="main">Movies</div>
+        <div className="main">{movies[0].name}</div>
       </header>
     </div>
   );
