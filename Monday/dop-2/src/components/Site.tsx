affd0cbf6b7c080d4316cb982f032312236393d8
IJTINK7 <ijtink7@mail.ru>
2023-04-28 20:50:31 +0300

    Changing showing of components

--- Monday/dop-2/src/components/Site.tsx
+++ Monday/dop-2/src/components/Site.tsx
@@ -5,6 +5,7 @@ import {PageOne} from "./pages/PageOne";
 import {PageTwo} from "./pages/PageTwo";
 import {PageThree} from "./pages/PageThree";
 import {Error404} from "./pages/Error404";
+import {Page} from "./pages/Page";
 
 
 export const Site = () => {
@@ -13,17 +14,17 @@ export const Site = () => {
             <div className={styles.header}><h1>HEADER</h1></div>
             <div className={styles.body}>
                 <div className={styles.nav}>
-                    <div><NavLink className={({isActive})=>isActive ? styles.active : styles.nav} to={"/page1"}>Page1</NavLink></div>
-                    <div><NavLink className={({isActive})=>isActive ? styles.active : styles.nav} to={"/page2"}>Page2</NavLink></div>
-                    <div><NavLink className={({isActive})=>isActive ? styles.active : styles.nav} to={"/page3"}>Page3</NavLink></div>
+                    <div><NavLink className={({isActive})=>isActive ? styles.active : styles.nav} to={"/page/0"}>Page1</NavLink></div>
+                    <div><NavLink className={({isActive})=>isActive ? styles.active : styles.nav} to={"/page/1"}>Page2</NavLink></div>
+                    <div><NavLink className={({isActive})=>isActive ? styles.active : styles.nav} to={"/page/2"}>Page3</NavLink></div>
                     <p><a href="/page3">Native JS Link</a></p>
                 </div>
                 <div className={styles.content}>
                     <Routes>
-                        <Route path={"/"} element={<Navigate to={"/page1"} />}/>
-                        <Route path={"/page1"} element={<PageOne/>}/>
-                        <Route path={"/page2"} element={<PageTwo/>}/>
-                        <Route path={"/page3"} element={<PageThree/>}/>
+                        <Route path={"/"} element={<Navigate to={"/page/0"} />}/>
+                        <Route path={"/page1"} element={<Page/>}/>
+                        <Route path={"/page2"} element={<Page/>}/>
+                        <Route path={"/page3"} element={<Page/>}/>
                         <Route path={"*"} element={<Error404/>}/>
                     </Routes>
                 </div>
