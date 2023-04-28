51e76eebaf47a5d9d6ba4fd1b2b1e12d40d1d78a
IJTINK7 <ijtink7@mail.ru>
2023-04-28 20:25:16 +0300

    Changing Routes

---
+++ Monday/dop-2/src/components/Site.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {Navigate, Route, Routes} from 'react-router-dom';
+import styles from './Site.module.css'
+import {PageOne} from "./pages/PageOne";
+import {PageTwo} from "./pages/PageTwo";
+import {PageThree} from "./pages/PageThree";
+import {Error404} from "./pages/Error404";
+
+
+export const Site = () => {
+    return (
+        <div>
+            <div className={styles.header}><h1>HEADER</h1></div>
+            <div className={styles.body}>
+                <div className={styles.nav}>
+
+                </div>
+                <div className={styles.content}>
+                    <Routes>
+                        <Route path={"/"} element={<Navigate to={"/page1"} />}/>
+                        <Route path={"/page1"} element={<PageOne/>}/>
+                        <Route path={"/page2"} element={<PageTwo/>}/>
+                        <Route path={"/page3"} element={<PageThree/>}/>
+                        <Route path={"*"} element={<Error404/>}/>
+                    </Routes>
+                </div>
+            </div>
+        </div>
+    );
+};
+
