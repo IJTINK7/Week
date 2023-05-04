5b4c13f1f0d5c2a797805c8f3f119b2f4403ed83
IJTINK7 <ijtink7@mail.ru>
2023-05-04 17:29:25 +0300

    Using map method to "tasksFromServer" data array via "useEffect" hook

--- Monday/dop-2/src/App.tsx
+++ Monday/dop-2/src/App.tsx
@@ -139,7 +139,9 @@ function App() {
         }
     ]
 
-    useEffect(()=>{},[])
+    useEffect(() => {
+        tasksFromServer.map(el =>({...el}))
+    }, [])
 
     // let todolistId1 = v1();
     // let todolistId2 = v1();
@@ -199,45 +201,44 @@ function App() {
         // }
     }
     function removeTodolist(id: string) {
-    //     // засунем в стейт список тудулистов, id которых не равны тому, который нужно выкинуть
-    //     setTodolists(todolists.filter(tl => tl.id != id));
-    //     // удалим таски для этого тудулиста из второго стейта, где мы храним отдельно таски
-    //     delete tasks[id]; // удаляем св-во из объекта... значением которого являлся массив тасок
-    //     // засетаем в стейт копию объекта, чтобы React отреагировал перерисовкой
-    //     setTasks({...tasks});
-    // }
+        //     // засунем в стейт список тудулистов, id которых не равны тому, который нужно выкинуть
+        //     setTodolists(todolists.filter(tl => tl.id != id));
+        //     // удалим таски для этого тудулиста из второго стейта, где мы храним отдельно таски
+        //     delete tasks[id]; // удаляем св-во из объекта... значением которого являлся массив тасок
+        //     // засетаем в стейт копию объекта, чтобы React отреагировал перерисовкой
+        //     setTasks({...tasks});
+        // }
+        return (
+            <div className="App">
+                {
+                    todolists.map(tl => {
+                        let allTodolistTasks = tasks[tl.id];
+                        let tasksForTodolist = allTodolistTasks;
 
-    return (
-        <div className="App">
-            {
-                todolists.map(tl => {
-                    let allTodolistTasks = tasks[tl.id];
-                    let tasksForTodolist = allTodolistTasks;
+                        if (tl.filter === "active") {
+                            tasksForTodolist = allTodolistTasks.filter(t => t.isDone === false);
+                        }
+                        if (tl.filter === "completed") {
+                            tasksForTodolist = allTodolistTasks.filter(t => t.isDone === true);
+                        }
 
-                    if (tl.filter === "active") {
-                        tasksForTodolist = allTodolistTasks.filter(t => t.isDone === false);
-                    }
-                    if (tl.filter === "completed") {
-                        tasksForTodolist = allTodolistTasks.filter(t => t.isDone === true);
-                    }
+                        return <Todolist
+                            key={tl.id}
+                            id={tl.id}
+                            title={tl.title}
+                            tasks={tasksForTodolist}
+                            removeTask={removeTask}
+                            changeFilter={changeFilter}
+                            addTask={addTask}
+                            changeTaskStatus={changeStatus}
+                            filter={tl.filter}
+                            removeTodolist={removeTodolist}
+                        />
+                    })
+                }
 
-                    return <Todolist
-                        key={tl.id}
-                        id={tl.id}
-                        title={tl.title}
-                        tasks={tasksForTodolist}
-                        removeTask={removeTask}
-                        changeFilter={changeFilter}
-                        addTask={addTask}
-                        changeTaskStatus={changeStatus}
-                        filter={tl.filter}
-                        removeTodolist={removeTodolist}
-                    />
-                })
-            }
-
-        </div>
-    );
+            </div>
+        );
+    }
 }
-
 export default App;
